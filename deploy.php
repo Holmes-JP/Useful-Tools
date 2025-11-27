3aed64489e715b17c4ec957b4df9056f0206b95a
Holmes-MainPC <hoomaa1432@gmail.com>
2025-11-27 15:10:57 +0900

    Initial commit: Lightweight WASM tool server

---
+++ deploy.php
@@ -0,0 +1,34 @@
+<?php
+namespace Deployer;
+
+require 'recipe/common.php';
+
+// === プロジェクト設定 ===
+set('application', 'Useful-Tools');
+set('repository', 'git@github.com:Holmes-JP/Useful-Tools.git');
+set('git_tty', false);
+add('shared_files', ['.env']);
+
+// === デプロイ先のホスト設定 ===
+host('production')
+    ->setHostname('57.181.246.229')
+    ->setRemoteUser('ubuntu')
+    ->setIdentityFile('/root/key.pem')
+    ->set('deploy_path', '/var/www/useful-tools');
+
+// === ★追加: Composerインストールタスク ===
+task('deploy:vendors', function () {
+    run('cd {{release_path}} && composer install --verbose --prefer-dist --no-progress --no-interaction --optimize-autoloader');
+});
+
+
+// === タスク定義 ===
+task('deploy', [
+    'deploy:prepare',
+    'deploy:update_code', // コードを持ってくる
+    'deploy:vendors',     // ★ここでComposer installを実行！
+    'deploy:shared',      // .envなどの共有リンク
+    'deploy:publish',     // 公開（シンボリックリンク切り替え）
+]);
+
+after('deploy:failed', 'deploy:unlock');
